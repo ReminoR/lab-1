9c0c3315511f85c7c4c2c946edd258dd7bc79725
ReminoR <victor.shamakov@gmail.com>
2017-11-17 17:45:52 +0700

    Lab.work #1. Version 2

--- index.py
+++ index.py
@@ -4,16 +4,16 @@ import re
 #http://www.tsu.ru/help/contacts.php
 #http://space-monkey.ru
 #http://mosigra.ru
-site = requests.get('http://www.tsu.ru/help/contacts.php')
-content = site.text.split(' ')
+site = requests.get('http://mosigra.ru')
+content = site.text.split(' ,')
 
+pattern = re.compile(r'[A-Za-z0-9]+\.*[A-Za-z0-9_-]+\.*[A-Za-z0-9_-]+@[A-Za-z0-9_-]+\.*[A-Za-z0-9_-]*\.[A-Za-z0-9]{2,10}')
 result = []
 
-for value in content:
-    mask = re.findall(r'mailto:(\w+@\w+.\w+)', value)
-    if mask:
-        result.append(*mask)
-        result = list(set(result))
+for i in range(len(content)):
+	result = pattern.findall(content[i])
+result = list(set(result))
+
 
 for i in range(len(result)):
-    print(i+1,result[i]) 
+    print(i+1, result[i])
