421da94eabc817fcc2990a37f964e72503522a14
ReminoR <victor.shamakov@gmail.com>
2018-01-18 15:36:34 +0700

    Added exceptions and exceptions for @Mail.Ru

--- index.py
+++ index.py
@@ -4,13 +4,17 @@ import re
 #http://www.tsu.ru/help/contacts.php
 #http://space-monkey.ru
 #http://mosigra.ru
-site = requests.get('http://mosigra.ru').text
+#http://my.mail.ru
+site = requests.get('http://my.mail.ru').text
 pattern = re.compile(r'\w+\.*-*_*\w*\.*-*_*\w*@\w+\.*-*_*\w*\.\w{2,10}')
 emails = list(set(pattern.findall(site)))
+exceptions = [] #добавляем исключения
+pattern_mail_ru = re.compile(r'@Mail\.')
 
 
 with open('email.csv', 'w') as file:
-	for i in emails:
-		file.write(i)
-		file.write('\n')
+	for email in emails:
+		if not pattern_mail_ru.findall(email) and email not in exceptions:
+			file.write(email + '\n')
+			print(email)
 	file.close()
